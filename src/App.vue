d3777382cfe360a8e79f3731aa79ac330a9594d1
tsingwong <919916459@qq.com>
2017-11-22 16:58:57 +0800

    add leftNav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <div id="app">
-    <i class="icon iconfont icon-huiyuan"></i>
-    <i class="icon iconfont icon-shezhi"></i>
-    <i class="icon iconfont icon-dianpu"></i>
-    <i class="icon iconfont icon-shumashouji"></i>
-    <router-view/>
-  </div>
+    <div id="app">
+        <leftNav></leftNav>
+        <div class="main">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    name: 'app'
-};
+    import leftNav from './components/common/leftNav.vue';
+    export default {
+        name: 'app',
+        components: {
+            leftNav
+        },
+    };
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+    .main {
+        float: left;
+        width:95%;
+        background-color: #EFF2F7;
+        height: 100%;
+        overflow: hidden;
+    }
 </style>
--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="pos">
-        Hello CashierDesk!
     </div>
 </template>
 
