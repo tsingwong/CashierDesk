c15c4926afba47715280c0baab219a1b87bc5f81
tsingwong <919916459@qq.com>
2017-11-25 18:39:48 +0800

    add some methods

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -4,7 +4,7 @@
             <el-col :span="7" class="pos-order" ref="order-list" id="order-list">
                 <el-tabs type="card">
                     <el-tab-pane label="点餐">
-                        <el-table :data="tableData" border show-summary style="width: 100%">
+                        <el-table :data="tableData" border style="width: 100%">
                             <el-table-column prop="goodName" label="商品名称">
 
                             </el-table-column>
@@ -16,15 +16,16 @@
                             </el-table-column>
                             <el-table-column fixed="right" label="操作">
                                 <template slot-scope="scope">
-                                    <el-button type="text" size="small">
+                                    <el-button type="text" size="small" @click="delSingleGood(scope.row)">
                                         删除
                                     </el-button>
-                                    <el-button type="text" size="small">
+                                    <el-button type="text" size="small" @click="addOrderList(scope.row)">
                                         增加
                                     </el-button>
                                 </template>
 </el-table-column>
 </el-table>
+<div class="total"><span>数量：<small></small>{{totalCount}}</span> <span><small>金额：</small>{{totalMoney}}元</span></div>
 <div class="pos-btn">
     <el-button type="primary">
         挂单
@@ -50,7 +51,7 @@
         <div class="common-goods-title">常用商品</div>
         <div class="common-goods-list">
             <ul>
-                <li v-for="goods in commongoods">
+                <li v-for="goods in commongoods" @click="addOrderList(goods)">
                     <span>{{goods.goodsName}}</span>
                     <span class="common-goods-price">￥{{goods.price}}</span>
                 </li>
@@ -62,7 +63,7 @@
         <el-tabs type="border-card" value="1">
             <el-tab-pane label="汉堡" name="1">
                 <ul class="cook-list">
-                    <li v-for="food in type0Foods">
+                    <li v-for="food in type0Foods" @click="addOrderList(food)">
                         <span class="food-img"><img :src="food.goodsImg" width="100%" /></span>
                         <span class="food-name">{{food.goodsName}}</span>
                         <span class="food-price">￥{{food.price}}元</span>
@@ -71,7 +72,7 @@
             </el-tab-pane>
             <el-tab-pane label="小吃" name="2">
                 <ul class="cook-list">
-                    <li v-for="food in type1Foods">
+                    <li v-for="food in type1Foods" @click="addOrderList(food)">
                         <span class="food-img"><img :src="food.goodsImg" width="100%" /></span>
                         <span class="food-name">{{food.goodsName}}</span>
                         <span class="food-price">￥{{food.price}}元</span>
@@ -80,7 +81,7 @@
             </el-tab-pane>
             <el-tab-pane label="饮料" name="3">
                 <ul class="cook-list">
-                    <li v-for="food in type2Foods">
+                    <li v-for="food in type2Foods" @click="addOrderList(food)">
                         <span class="food-img"><img :src="food.goodsImg" width="100%" /></span>
                         <span class="food-name">{{food.goodsName}}</span>
                         <span class="food-price">￥{{food.price}}元</span>
@@ -89,7 +90,7 @@
             </el-tab-pane>
             <el-tab-pane label="套餐" name="4">
                 <ul class="cook-list">
-                    <li v-for="food in type3Foods">
+                    <li v-for="food in type3Foods" @click="addOrderList(food)">
                         <span class="food-img"><img :src="food.goodsImg" width="100%" /></span>
                         <span class="food-name">{{food.goodsName}}</span>
                         <span class="food-price">￥{{food.price}}元</span>
@@ -110,58 +111,34 @@
         created() {
             axios.get('http://localhost:8083/commongoods')
                 .then(res => {
-                    // eslint-disable-next-line
-                    console.log(res);
                     this.commongoods = res.data;
                 })
+                // eslint-disable-next-line
                 .catch(err => {
-                    // eslint-disable-next-line
-                    console.log(err);
                     alert('NetWork Error');
                 });
             axios.get('http://localhost:8083/typeGoods')
                 .then(res => {
-                    // eslint-disable-next-line
-                    console.log(res);
-
                     this.type0Foods = res.data[0];
                     this.type1Foods = res.data[1];
                     this.type2Foods = res.data[2];
                     this.type3Foods = res.data[3];
                 })
+                // eslint-disable-next-line
                 .catch(err => {
-                    // eslint-disable-next-line
-                    console.log(err);
                     alert('NetWork Error');
                 });
         },
         data() {
             return {
-                tableData: [{
-                    goodName: '可口可乐',
-                    price: 8,
-                    count: 1
-                }, {
-
-                    goodName: '香辣鸡腿堡',
-                    price: 15,
-                    count: 1
-                }, {
-
-                    goodName: '爱心薯条',
-                    price: 8,
-                    count: 1
-                }, {
-
-                    goodName: '甜筒',
-                    price: 8,
-                    count: 1
-                }],
+                tableData: [],
                 commongoods: [],
                 type0Foods: [],
                 type1Foods: [],
                 type2Foods: [],
-                type3Foods: []
+                type3Foods: [],
+                totalCount: 0,
+                totalMoney: 0,
             };
         },
         mounted: function () {
@@ -174,6 +151,51 @@
              */
             let orderList = document.querySelector('#order-list');
             orderList && (orderList.style.height = orderHeight + 'px');
+        },
+        methods: {
+            addOrderList(goods) {
+                // 商品是否存在于订单列表中，
+                // 无，新增。有，则数量 +1
+                let isHave = false;
+                for (let i of this.tableData) {
+                    i.goodsId === goods.goodsId && (isHave = true);
+                }
+
+                if (isHave) {
+                    this.tableData.filter(o => {
+                        o.goodsId === goods.goodsId && o.count++;
+                    });
+                } else {
+                    let newGoods = {
+                        goodsId: goods.goodsId,
+                        goodName: goods.goodsName,
+                        price: goods.price,
+                        count: 1
+                    };
+                    // let newGoods;
+                    // {newGoods.goodsId, newGoods.goodName: goodsName, newGoods.price} = goods;
+                    // newGood.count = 1;
+                    this.tableData.push(newGoods);
+                }
+                this.total();
+            },
+            delSingleGood (goods) {
+                const index = this.tableData.indexOf(goods);
+                goods.count -= 1;
+                goods.count === 0
+                && index > -1
+                && this.tableData.splice(index, 1);
+                this.total();
+            },
+            total () {
+                this.totalCount = 0;
+                this.totalMoney = 0;
+                this.tableData.length
+                && this.tableData.forEach((element) => {
+                    this.totalCount += element.count;
+                    this.totalMoney += element.count * element.price;
+                });
+            }   
         }
     };
 
@@ -204,6 +226,7 @@
         padding: 10px;
         margin: 10px;
         background-color: white;
+        cursor: pointer;
     }
 
     .common-goods-price {
@@ -224,6 +247,7 @@
         padding: 2px;
         float: left;
         margin: 2px;
+        cursor: pointer;
     }
 
     .cook-list li span {
@@ -236,7 +260,7 @@
     }
 
     .food-name {
-        font-size: 18px;
+        font-size: 16px;
         padding-left: 10px;
         color: brown;
     }
@@ -246,5 +270,13 @@
         padding-left: 10px;
         padding-top: 10px;
     }
-
+    .total {
+        text-align: left;
+        background-color: #fff;
+        padding: 10px;
+        border-bottom: 1px solid #D3dce6;
+    }
+    .total span {
+        margin-left: 10px;
+    }
 </style>
